2bc443c0a6ba7f98b1a45435d14ff4fbf80cd43a
celayaz61 <35407080+celayaz61@users.noreply.github.com>
2020-05-19 17:24:56 -0400

    3rd Commit

--- src/deneme.java
+++ src/deneme.java
@@ -1,7 +1,7 @@
 public class deneme {
 
     public static void main(String[] args) {
-        System.out.println("hello world hello world hi world");
+        System.out.println("hello world hello world hi world fsdfsd");
     }
 
     public boolean isDenemek(){
